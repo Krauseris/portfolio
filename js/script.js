d2f85be91f4c0b8a62df00d84044184c8af61fef
Krausers <59839326+Krausers@users.noreply.github.com>
2020-07-16 23:53:13 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$.fn.timedDisable = function(time) {
+    if (time == null) { time = 500; }
+    return $(this).each(function() {
+        $(this).attr('disabled', 'disabled');
+        var disabledElem = $(this);
+        setTimeout(function() {
+            disabledElem.removeAttr('disabled');
+        }, time);
+    });
+};
+
+function send() {
+    document.location.href = "https://mail.google.com/"
+}
+
+const but = document.getElementById('send');
+
+but.onclick = send; 
+
+$(function() {
+    $("#butmenu").on('click', function() {
+        $("#menu, #closemenu").toggle();
+        $("#hidemenu").slideToggle(500);
+        $("#translate_nav").fadeToggle(600);
+        $('#butmenu').timedDisable();
+        $('body').toggleClass('stops');
+    });
+    $(".nonstop").on('click', function() {
+        $("#menu, #closemenu").toggle();
+        $("#hidemenu").toggle();
+        $("#translate_nav").toggle();
+        $('body').toggleClass('stops');
+    });
+});
